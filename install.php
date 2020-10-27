d4f697270708fd791e2eccad4b48e3da79012ad0
DayWalker[MM] <emulilo17@gmail.com>
2020-10-27 14:13:07 +0200

    Add files via upload

--- install.php
+++ install.php
@@ -4,7 +4,7 @@
 $shop = $_GET['shop'];
 $api_key = "70bdfe1c895cdcbf82f069556f38baa0";
 $scopes = "read_orders,write_products,read_script_tags,write_script_tags";
-$redirect_uri = "https://b5116285d2d6.ngrok.io/generate_token.php";
+$redirect_uri = "https://php-shopify-app.herokuapp.com/generate_token.php";
 
 // Build install/approval URL to redirect to
 $install_url = "https://" . $shop . ".myshopify.com/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
