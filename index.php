b050e42128152b28b3fc2633783df31104b6cb40
DayWalker[MM] <emulilo17@gmail.com>
2020-10-27 17:11:02 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -8,8 +8,13 @@ $serializeArray = serialize($request);
 $requests = array_diff_key($requests, array('hmac' => ''));
 ksort($requests);
 
+$parsedUrl = parse_url('https://'.$params['shop']);
+$host = explode('.', $parsedUrl['host']);
+$subdomain = $host[0];
+
+$shop = $subdomain;
 $token = "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";
-$shop = "redirect-to-checkout.myshopify.com";
+//$shop = "redirect-to-checkout.myshopify.com";
 
 $array = array(
 	'script_tag' => array(
@@ -18,7 +23,7 @@ $array = array(
 	)
 );
 
-$scriptTag = shopify_call($token, $shop, "/admin/api/2020-10/script_tags.json", $array, 'POST');
+$scriptTag = shopify_call($token, $shop, "/admin/api/2019-07/script_tags.json", $array, 'POST');
 $scriptTag = json_decode($scriptTag['response'], JSON_PRETTY_PRINT);
 
 
