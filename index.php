fb5d9ae4bb6c1baf815007b7566e58467f4873e2
DayWalker[MM] <emulilo17@gmail.com>
2020-10-27 15:12:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,10 +8,10 @@ $serializeArray = serialize($request);
 $requests = array_diff_key($requests, array('hmac' => ''));
 ksort($requests);
 
-$token = "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";
-$subdomain = $host[0];
-$shop = $subdomain;
-//$shop = "redirect-to-checkout.myshopify.com";
+//$token = "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";
+//$subdomain = $host[0];
+//$shop = $subdomain;
+$shop = "redirect-to-checkout.myshopify.com";
 
 $collectionList = shopify_call($token, $shop, "/admin/api/2020-10/custome_collection.json", array(), 'GET');
 $collectionList = json_decode($collectionList['response'], JSON_PRETTY_PRINT);
