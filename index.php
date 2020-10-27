7cf73efdce163b008956097faaf89cbad2590567
DayWalker[MM] <emulilo17@gmail.com>
2020-10-27 23:20:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,5 @@
 <?php
 
-require_once("inc/functions.php");
-
-$request = $_GET;
-$hmac = $_GET['hmac'];
-$serializeArray = serialize($request);
-$requests = array_diff_key($requests, array('hmac' => ''));
-ksort($requests);
-
-$parsedUrl = parse_url('https://'.$params['shop']);
-$host = explode('.', $parsedUrl['host']);
-$subdomain = $host[0];
-
-$shop = $subdomain;
-$token = "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";
-//$shop = "redirect-to-checkout.myshopify.com";
-
-$array = array(
-	'script_tag' => array(
-		'event' => 'onload', 
-		'src' => 'https://ephraim17.github.io/Blue-Dragonfly/script.js'
-	)
-);
-
-$scriptTag = shopify_call($token, "https://daywalkers-app-test-store.myshopify.com/", "/admin/api/2020-10/script_tags.json", $array, 'POST');
-$scriptTag = json_decode($scriptTag['response'], JSON_PRETTY_PRINT);
-
-
-?>
-
 <!DOCTYPE html>
 <html>
     <head>
