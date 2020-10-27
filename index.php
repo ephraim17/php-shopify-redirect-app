ef722e8e531828d270216be8ffa957164d712007
DayWalker[MM] <emulilo17@gmail.com>
2020-10-27 16:46:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,8 +9,6 @@ $requests = array_diff_key($requests, array('hmac' => ''));
 ksort($requests);
 
 $token = "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";
-//$subdomain = $host[0];
-//$shop = $subdomain;
 $shop = "redirect-to-checkout.myshopify.com";
 
 $collectionList = shopify_call($token, $shop, "/admin/api/2020-10/custome_collection.json", array(), 'GET');
