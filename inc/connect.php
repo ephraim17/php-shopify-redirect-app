99d692af25844d755997b82645338d95ec8aab72
DayWalker[MM] <emulilo17@gmail.com>
2020-10-28 19:09:59 +0200

    Added database

---
+++ inc/connect.php
@@ -0,0 +1,13 @@
+<?php
+
+$host = "localhost";
+$username = "bfdbe3822d6e93";
+$password = "fa50e3e6";
+$dbname = "bfdbe3822d6e93";
+
+$conn = mysqli_connect( $host, $username, $password, $dbname );
+
+if ( !$conn ) {
+
+    die( "Connect was not successful: " . mysqli_connect_error() );
+}
