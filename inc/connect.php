a009bade826f62a148f895436f75c28ef18decb9
DayWalker[MM] <emulilo17@gmail.com>
2020-10-28 19:18:31 +0200

    Add files via upload

--- inc/connect.php
+++ inc/connect.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "php-shopify-app.herokuapp.com";
+$host = "mysql://bfdbe3822d6e93:fa50e3e6@eu-cdbr-west-03.cleardb.net/heroku_c9f515d96d37b64?reconnect=true";
 $username = "bfdbe3822d6e93";
 $password = "fa50e3e6";
-$dbname = "bfdbe3822d6e93";
+$dbname = "heroku_c9f515d96d37b64";
 
 $conn = mysqli_connect( $host, $username, $password, $dbname );
 
