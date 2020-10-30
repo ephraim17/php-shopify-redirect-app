7a185f5a4c52142db0e2d882337daf0c25b8c53a
DayWalker[MM] <emulilo17@gmail.com>
2020-10-30 13:51:25 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
 require_once("inc/functions.php");
+require_once("inc/connect.php");
 
 
 $requests = $_GET;
@@ -93,15 +94,11 @@ $scriptTag = json_decode($scriptTag['response'], JSON_PRETTY_PRINT);
  <!DOCTYPE html>
  <html>
  <head>
- 	<title>Shopify Example A2pp</title>
+ 	<title>Shopify Example App</title>
  </head>
  <body>
  	<h1>Shopify Example App</h1>
  	<img src="<?php echo $image; ?>" style="width:250px;">
  	<p><?php echo $title; ?></p>
-	 
-	 <script>
-		 console.log('This is my location ' + location.href);
-	 </script>
  </body>
  </html>
