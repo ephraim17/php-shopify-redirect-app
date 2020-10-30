334ce8af21a63b3eaba9c615232812fa4c370b79
DayWalker[MM] <emulilo17@gmail.com>
2020-10-30 13:19:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php 
 require_once("inc/functions.php");
-require_once("inc/connect.php");
 
 
 $requests = $_GET;
@@ -15,25 +14,23 @@ $shop = $row['store_url'];
 $sql = "SELECT * FROM example_table WHERE store_url='" . $requests['shop'] . "' LIMIT 1";
 $result = mysqli_query( $conn, $sql );
 $row = mysqli_fetch_assoc($result);
-
 //Product and Product Images
-/*
 $image = "";
 $title = "";
 
-$collectionList = shopify_call($token, $shop, "/admin/api/2020-10/custom-collections.json", array(), 'GET');
+$collectionList = shopify_call($token, $shop, "/admin/api/2020-04/custom-collections.json", array(), 'GET');
 $collectionList = json_decode($collectionList['response'], JSON_PRETTY_PRINT);
 $collection_id = $collectionList['custom_collections'][0]['id'];
 
-$collects = shopify_call($token, $shop, "/admin/api/2020-10/collects.json", array("collection_id"=>$collection_id), "GET");
+$collects = shopify_call($token, $shop, "/admin/api/2020-04/collects.json", array("collection_id"=>$collection_id), "GET");
 $collects = json_decode($collects['response'], JSON_PRETTY_PRINT);
 
 foreach ($collects as $collect) {
 	foreach($collect as $key => $value) {
-		$products = shopify_call($token, $shop, "/admin/api/2020-10/products/" . $value['product_id'] . ".json", array(), "GET");
+		$products = shopify_call($token, $shop, "/admin/api/2020-04/products/" . $value['product_id'] . ".json", array(), "GET");
 		$products = json_decode($products['response'], JSON_PRETTY_PRINT);
 
-		$images = shopify_call($token, $shop, "/admin/api/2020-10/products/" . $value['product_id'] . "/images.json", array(), "GET");
+		$images = shopify_call($token, $shop, "/admin/api/2020-04/products/" . $value['product_id'] . "/images.json", array(), "GET");
 		$images = json_decode($images['response'], JSON_PRETTY_PRINT);
 
 
@@ -44,7 +41,7 @@ foreach ($collects as $collect) {
 }
 
 // Based on 4th video
-$theme = shopify_call($token, $shop, "/admin/api/2020-10/themes.json", array(), "GET");
+$theme = shopify_call($token, $shop, "/admin/api/2020-04/themes.json", array(), "GET");
 $theme = json_decode($theme['response'], JSON_PRETTY_PRINT);
 
 echo print_r($theme);
@@ -62,14 +59,14 @@ foreach ($theme as $curr_theme) {
  					"value" => "<script>document.querySelector('.h1').innerText = 'SHOPIFY 10';</script>"
    				)
 			);*/
-/*
-			$assets = shopify_call($token, $shop, "/admin/api/2020-10/themes/" . $value['id'] . "/assets.json", $array, "PUT");
+
+			$assets = shopify_call($token, $shop, "/admin/api/2020-04/themes/" . $value['id'] . "/assets.json", $array, "PUT");
 		    $assets = json_decode($assets['response'], JSON_PRETTY_PRINT);
 
 		}
 	}
 }
-*/
+
 
 
 $script_array = array(
@@ -79,7 +76,7 @@ $script_array = array(
  )
 );
 
-$scriptTag = shopify_call($token, $shop, "/admin/api/unstable/script_tags.json", $script_array, "POST");
+$scriptTag = shopify_call($token, $shop, "/admin/api/2020-04/script_tags.json", $script_array, "POST");
 $scriptTag = json_decode($scriptTag['response'], JSON_PRETTY_PRINT);
 
 
@@ -92,8 +89,8 @@ $scriptTag = json_decode($scriptTag['response'], JSON_PRETTY_PRINT);
  	<title>Shopify Example App</title>
  </head>
  <body>
- 	<h1>Shopify Example App V2</h1>
- 	<img src="<?php echo $scriptTag['response']; ?>" style="width:250px;">
+ 	<h1>Shopify Example App2</h1>
+ 	<img src="<?php echo $image; ?>" style="width:250px;">
  	<p><?php echo $title; ?></p>
  </body>
  </html>
