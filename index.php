a32a3a92c6757da5223d62a15919b453a247ce27
DayWalker[MM] <emulilo17@gmail.com>
2020-10-31 22:21:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -21,6 +21,11 @@ $var = "Hello, I am string using replaced ";
 $token = $row['access_token'];
 $shop = str_replace(".myshopify.com", "", $row['store_url']);
 
+echo $var;
+echo $row['access_token'];
+echo $row['store_url'];
+echo str_replace(".myshopify.com", "", $row['store_url']);
+
 //Product and Product Images
 $image = "";
 $title = "";
@@ -98,10 +103,23 @@ $scriptTag = json_decode($scriptTag['response'], JSON_PRETTY_PRINT);
   rel="stylesheet"
   href="https://unpkg.com/@shopify/polaris@5.0.0/dist/styles.css"
 />
+
+<style>
+.center {
+  text-align: center;
+}
+</style>
+
+
  </head>
  <body>
- 	<h1>This app is now redirecting users to checkout</h1><div style="--top-bar-background:#00848e; --top-bar-background-lighter:#1d9ba4; --top-bar-color:#f9fafb; --p-frame-offset:0px;"><span class="Polaris-Spinner Polaris-Spinner--colorTeal Polaris-Spinner--sizeLarge"><svg viewBox="0 0 44 44" xmlns="http://www.w3.org/2000/svg">
+
+ <div class="center">
+ <h1>This app is now redirecting users to checkout</h1><div style="--top-bar-background:#00848e; --top-bar-background-lighter:#1d9ba4; --top-bar-color:#f9fafb; --p-frame-offset:0px;"><span class="Polaris-Spinner Polaris-Spinner--colorTeal Polaris-Spinner--sizeLarge"><svg viewBox="0 0 44 44" xmlns="http://www.w3.org/2000/svg">
       <path d="M15.542 1.487A21.507 21.507 0 00.5 22c0 11.874 9.626 21.5 21.5 21.5 9.847 0 18.364-6.675 20.809-16.072a1.5 1.5 0 00-2.904-.756C37.803 34.755 30.473 40.5 22 40.5 11.783 40.5 3.5 32.217 3.5 22c0-8.137 5.3-15.247 12.942-17.65a1.5 1.5 0 10-.9-2.863z"></path>
     </svg></span><span role="status"><span class="Polaris-VisuallyHidden">Spinner example</span></span></div>
+</div>
+
+ 	
  </body>
  </html>
