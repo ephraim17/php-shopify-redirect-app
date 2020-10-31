7657820f94a025c844e1939d0924f37b07bb77f8
DayWalker[MM] <emulilo17@gmail.com>
2020-10-31 22:30:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ $row = mysqli_fetch_assoc($result);
 $token1 = $row['access_token'];
 $shop1 = $row['store_url'];
 
-// $var = "Hello, I am string using replaced ";
+$var = "Hello, I am string using replaced ";
 
 $token = $row['access_token'];
 $shop = str_replace(".myshopify.com", "", $row['store_url']);
@@ -25,6 +25,25 @@ $shop = str_replace(".myshopify.com", "", $row['store_url']);
 // echo $row['access_token'];
 // echo $row['store_url'];
 // echo str_replace(".myshopify.com", "", $row['store_url']);
+
+
+// $recurring_array = array(
+// 	'recurring_application_charge' => array(
+// 		'name' => 'Example Plan',
+// 		'test' => true,  //remove this line before sending to app store
+// 		'price' => 4.99,
+// 		'return_url' => "https://" . $row['store_url'] . "/admin/apps/php-my-app/?" . $_SERVER['QUERY_STRING']
+// 	)
+// );
+
+
+
+// $charge = shopify_call($token, $shop, "/admin/api/2020-10/recurring_application_charges.json", $recurring_array, 'POST');
+// $charge = json_decode($charge['response'], JSON_PRETTY_PRINT);
+
+
+// header('Location: ' . $charge['recurring_application_charge']['confirmation_url'] );
+// exit();
 
 //Product and Product Images
 $image = "";
@@ -95,6 +114,7 @@ $scriptTag = json_decode($scriptTag['response'], JSON_PRETTY_PRINT);
 
  ?>
 
+ 
  <!DOCTYPE html>
  <html>
  <head>
@@ -116,7 +136,7 @@ $scriptTag = json_decode($scriptTag['response'], JSON_PRETTY_PRINT);
  <body>
 
  <div class="center">
- <h1>This app is now redirecting</h1><div style="--top-bar-background:#00848e; --top-bar-background-lighter:#1d9ba4; --top-bar-color:#f9fafb; --p-frame-offset:0px;"><span class="Polaris-Spinner Polaris-Spinner--colorTeal Polaris-Spinner--sizeLarge"><svg viewBox="0 0 44 44" xmlns="http://www.w3.org/2000/svg">
+ <h1>Now redirecting</h1><div style="--top-bar-background:#00848e; --top-bar-background-lighter:#1d9ba4; --top-bar-color:#f9fafb; --p-frame-offset:0px;"><span class="Polaris-Spinner Polaris-Spinner--colorTeal Polaris-Spinner--sizeLarge"><svg viewBox="0 0 44 44" xmlns="http://www.w3.org/2000/svg">
       <path d="M15.542 1.487A21.507 21.507 0 00.5 22c0 11.874 9.626 21.5 21.5 21.5 9.847 0 18.364-6.675 20.809-16.072a1.5 1.5 0 00-2.904-.756C37.803 34.755 30.473 40.5 22 40.5 11.783 40.5 3.5 32.217 3.5 22c0-8.137 5.3-15.247 12.942-17.65a1.5 1.5 0 10-.9-2.863z"></path>
     </svg></span><span role="status"><span class="Polaris-VisuallyHidden">Spinner example</span></span></div>
 </div>
