ff8f58cd4dcfc0089dd9f216db861c8f44ca1ec0
DayWalker[MM] <emulilo17@gmail.com>
2020-11-01 14:46:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -104,6 +104,7 @@ $scriptTag = json_decode($scriptTag['response'], JSON_PRETTY_PRINT);
       <path d="M15.542 1.487A21.507 21.507 0 00.5 22c0 11.874 9.626 21.5 21.5 21.5 9.847 0 18.364-6.675 20.809-16.072a1.5 1.5 0 00-2.904-.756C37.803 34.755 30.473 40.5 22 40.5 11.783 40.5 3.5 32.217 3.5 22c0-8.137 5.3-15.247 12.942-17.65a1.5 1.5 0 10-.9-2.863z"></path>
     </svg></span><span role="status"><span class="Polaris-VisuallyHidden">Spinner example</span></span></div>
 </div>
+	 <script src="//code.tidio.co/emvrdv8i57vs6jajetqwiei3azx8t5wf.js" async></script>
 
  	
  </body>
