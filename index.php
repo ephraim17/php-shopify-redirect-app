6513ac528bd9afb1e41d05372983caef1f58b0d4
DayWalker[MM] <emulilo17@gmail.com>
2020-11-01 21:22:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -18,9 +18,6 @@ $shop1 = $row['store_url'];
 
 $var = "Hello, I am string using replaced ";
 
-$token = "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";
-$shop = "redirect-to-checkout";
-
 $token = $row['access_token'];
 $shop = str_replace(".myshopify.com", "", $row['store_url']);
 
