c74022573943c558e3c53aefad3bae290b3e3b37
DayWalker[MM] <emulilo17@gmail.com>
2020-11-02 16:19:23 +0200

    Added mandatory shopify webhook endpoints

---
+++ webhooks/customer_data_request.php
@@ -0,0 +1,20 @@
+<?php
+
+define('SHOPIFY_APP_SECRET', 'my_shared_secret');
+
+
+
+function verify_webhook($data, $hmac_header)
+{
+  $calculated_hmac = base64_encode(hash_hmac('sha256', $data, SHOPIFY_APP_SECRET, true));
+  return hash_equals($hmac_header, $calculated_hmac);
+}
+
+
+
+$hmac_header = $_SERVER['HTTP_X_SHOPIFY_HMAC_SHA256'];
+$data = file_get_contents('php://input');
+$verified = verify_webhook($data, $hmac_header);
+error_log('Webhook verified: '.var_export($verified, true)); //check error.log to see the result
+
+
