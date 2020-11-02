493b6481040ed53b296a982d36ed20d3657bafee
DayWalker[MM] <emulilo17@gmail.com>
2020-11-02 14:40:41 +0200

    Update install.php

--- install.php
+++ install.php
@@ -7,7 +7,7 @@ $scopes = "read_customers,write_customers,read_orders,write_products,read_themes
 $redirect_uri = "https://php-shopify-app.herokuapp.com/generate_token.php";
 
 // Build install/approval URL to redirect to
-$install_url = "https://" . $shop . ".myshopify.com/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
+$install_url = "https://" . $shop . "admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
 
 // Redirect
 header("Location: " . $install_url);
