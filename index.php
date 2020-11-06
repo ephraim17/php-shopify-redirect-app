1c5b37a3d42adac8ad4ac10acdff606418d71171
DayWalker[MM] <emulilo17@gmail.com>
2020-11-06 19:54:56 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -18,6 +18,11 @@ $var = "Hello, I am string using replaced ";
 $token = $row['access_token'];
 $shop = str_replace(".myshopify.com", "", $row['store_url']);
 
+if ($token = false) {
+
+	header("Location: install.php?shop=" . $requests['shop']);
+
+  } 
 
 //Product and Product Images
 $image = "";
