e2db6f6b6fd93194cf543aa27afecc7247856753
DayWalker[MM] <emulilo17@gmail.com>
2020-11-06 20:36:51 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -19,9 +19,9 @@ $token = $row['access_token'];
 $shop = str_replace(".myshopify.com", "", $row['store_url']);
 
 if (empty($token)) {
-
 	header("Location: install.php?shop=" . $requests['shop']);
-  } 
+
+  };
 
 //Product and Product Images
 $image = "";
@@ -112,11 +112,11 @@ $scriptTag = json_decode($scriptTag['response'], JSON_PRETTY_PRINT);
  <body>
 
  <div class="center">
- <h1>Now redirecting customers. Leave a review</h1><div style="--top-bar-background:#00848e; --top-bar-background-lighter:#1d9ba4; --top-bar-color:#f9fafb; --p-frame-offset:0px;"><span class="Polaris-Spinner Polaris-Spinner--colorTeal Polaris-Spinner--sizeLarge"><svg viewBox="0 0 44 44" xmlns="http://www.w3.org/2000/svg">
+ <h1>Now redirecting customers.</h1><div style="--top-bar-background:#00848e; --top-bar-background-lighter:#1d9ba4; --top-bar-color:#f9fafb; --p-frame-offset:0px;"><span class="Polaris-Spinner Polaris-Spinner--colorTeal Polaris-Spinner--sizeLarge"><svg viewBox="0 0 44 44" xmlns="http://www.w3.org/2000/svg">
       <path d="M15.542 1.487A21.507 21.507 0 00.5 22c0 11.874 9.626 21.5 21.5 21.5 9.847 0 18.364-6.675 20.809-16.072a1.5 1.5 0 00-2.904-.756C37.803 34.755 30.473 40.5 22 40.5 11.783 40.5 3.5 32.217 3.5 22c0-8.137 5.3-15.247 12.942-17.65a1.5 1.5 0 10-.9-2.863z"></path>
     </svg></span><span role="status"><span class="Polaris-VisuallyHidden">Spinner example</span></span></div>
 </div>
-	 <script src="//code.tidio.co/emvrdv8i57vs6jajetqwiei3azx8t5wf.js" async></script>
+	<p>If this application worked for you, then please leave a review so it can help others as well. If you are having problems then please contact us via the chat bot and we will help you out!</p>	 <script src="//code.tidio.co/emvrdv8i57vs6jajetqwiei3azx8t5wf.js" async></script>
 
  	
  </body>
