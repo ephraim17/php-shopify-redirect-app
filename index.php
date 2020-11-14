126ca0905ab27509ed8c53e56a3384fb4b2f3a38
DayWalker[MM] <emulilo17@gmail.com>
2020-11-14 16:54:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -85,9 +85,15 @@ foreach ($theme as $curr_theme) {
 
 //echo $scriptTag_check;
 
-$scriptTag_checck = '1';
+$scriptTag_checck = 'scripttag check has been echoed';
 echo $scriptTag_checck;
 echo empty($scriptTag_checck);
+
+
+$ttheme = shopify_call($token, $shop, "/admin/api/2020-04/themes.json", array(), "GET");
+$ttheme = json_decode($ttheme['response'], JSON_PRETTY_PRINT);
+
+echo print_r($ttheme);
 
 
 
@@ -135,7 +141,7 @@ if $scriptTag contains ephraim mulilo script tag, dont run
  <body>
 
  <div class="center">
- <h1>i changed ahgain one more time for the almost last time. rip english. redirecting customers....</h1><div style="--top-bar-background:#00848e; --top-bar-background-lighter:#1d9ba4; --top-bar-color:#f9fafb; --p-frame-offset:0px;"><span class="Polaris-Spinner Polaris-Spinner--colorTeal Polaris-Spinner--sizeLarge">
+ <h1>i changed ahgain one more time for the almost last timrtgte. rip english. redirecting customers....</h1><div style="--top-bar-background:#00848e; --top-bar-background-lighter:#1d9ba4; --top-bar-color:#f9fafb; --p-frame-offset:0px;"><span class="Polaris-Spinner Polaris-Spinner--colorTeal Polaris-Spinner--sizeLarge">
 </div>
 	<p>If this application worked for you, then please leave a review so it can help others as well. If you are having problems then please contact us via the chat bot and we will help you out!</p>	 <script src="//code.tidio.co/emvrdv8i57vs6jajetqwiei3azx8t5wf.js" async></script>
 	<a href="upgrade.php?<?php echo $_SERVER['QUERY_STRING']; ?>" target="_blank">Upgrade</a>
