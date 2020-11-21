a10e8a496f29838dd61781c7faa6ef2ca344fa40
DayWalker[MM] <emulilo17@gmail.com>
2020-11-21 16:43:09 +0200

    Update install.php

--- install.php
+++ install.php
@@ -1,30 +1,4 @@
 <?php
-require_once("inc/functions.php");
-require_once("inc/connect.php");
-
-$requests = $_GET;
-$installshop = $_GET['shop'];
-$hmac = $_GET['hmac'];
-$serializeArray = serialize($requests);
-$requests = array_diff_key($requests, array( 'hmac' => '' ));
-ksort($requests);
-
-$sql = "SELECT * FROM example_table WHERE store_url='" . $requests['shop'] . "' LIMIT 1";
-$result = mysqli_query( $conn, $sql );
-$row = mysqli_fetch_assoc($result);
-
-$var = "Hello, I am string using replaced ";
-
-$token = $row['access_token'];
-$shop = str_replace(".myshopify.com", "", $row['store_url']);
-
-
-$installshop = $_GET['shop'];
-
-if ($token) {
-	header('Location: https://'.$installshop.'/admin/apps/php-my-app');
-  };
-
 
 // Set variables for our request
 $shop = $_GET['shop'];
