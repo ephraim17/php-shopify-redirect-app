2642e7a1ead3d76d08827b6e59c1c6a9b2b0bd4d
DayWalker[MM] <emulilo17@gmail.com>
2020-11-21 06:26:59 +0200

    Update install.php

--- install.php
+++ install.php
@@ -1,14 +1,44 @@
 <?php
 
+require_once("inc/functions.php");
+require_once("inc/connect.php");
+
+
+$requests = $_GET;
+$installshop = $_GET['shop'];
+$hmac = $_GET['hmac'];
+$serializeArray = serialize($requests);
+$requests = array_diff_key($requests, array( 'hmac' => '' ));
+ksort($requests);
+
+$sql = "SELECT * FROM example_table WHERE store_url='" . $requests['shop'] . "' LIMIT 1";
+$result = mysqli_query( $conn, $sql );
+$row = mysqli_fetch_assoc($result);
+
+$var = "Hello, I am string using replaced ";
+
+$token = $row['access_token'];
+$shop = str_replace(".myshopify.com", "", $row['store_url']);
+
+
 // Set variables for our request
 $shop = $_GET['shop'];
 $api_key = getenv('SHOPIFY_API_KEY');
-$scopes = "read_customers,write_customers,read_orders,write_products,read_themes,write_themes,write_script_tags";
+$scopes = "read_customers,read_orders,read_themes,write_themes,read_script_tags,write_script_tags";
 $redirect_uri = "https://auto-redirector-pro.herokuapp.com/generate_token.php";
 
 // Build install/approval URL to redirect to
 $install_url = "https://" . $shop . "/admin/oauth/authorize?client_id=" . $api_key . "&scope=" . $scopes . "&redirect_uri=" . urlencode($redirect_uri);
+$app_url = "https://" . $shop . "/admin/apps/php-my-app";
 
-// Redirect
-header("Location: " . $install_url);
+if (empty($token)) {
+
+    header("Location: " . $install_url);
+    
+  } else {
+
+    header("Location: " . $app_url);
+
+  };
+
 die();
